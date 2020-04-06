1c515d9990570bb407dae364abda9ca428efe458
shashankaryan <aryanshashank31@gmail.com>
2020-04-06 21:16:55 +0530

    Added Login API Signed-off-by: shashankaryan <aryanshashank31@gmail.com>

--- controllers/auth.py
+++ controllers/auth.py
@@ -1,5 +1,21 @@
 from flask import Blueprint, request, current_app
+from services.auth import AuthService
 from dto.result import ResponseDto
-from utils import get_page_items
 
 auth_blueprint = Blueprint('auth_blueprint', __name__)
+
+
+@auth_blueprint.route('/login/', methods=["POST"])
+def login():
+    email = request.form.get('email', '')
+    password = request.form.get('password', '')
+    remember = True if request.form.get('remember') else False
+    if not email.strip() or not password.strip() or not AuthService.validate_email(email):
+        return ResponseDto(status=-1, msg='Data Incomplete/Invalid.').to_json()
+
+    response = AuthService.login(email=email, password=password, remember=remember)
+    if not response['value']:
+        return ResponseDto(status=-1, msg=response['error']).to_json()
+    return ResponseDto(status=0, msg=response['msg'], result=response['data']).to_json()
+
+
